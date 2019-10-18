3d5f45ef8bab942c2713d789b77929e43bda9bbf
TMan123 <39570798+wtl6c@users.noreply.github.com>
2019-10-17 21:39:40 -0400

    Add files via upload

---
+++ mini-project-2-wtl6c/miniproject2test.d
@@ -0,0 +1,7 @@
+.\miniproject2test.o: MiniProject2Test.c
+.\miniproject2test.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\miniproject2test.o: OS.h
+.\miniproject2test.o: tm4c123gh6pm.h
+.\miniproject2test.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\miniproject2test.o: PLL.h
+.\miniproject2test.o: PORTE.h
---
+++ mini-project-2-wtl6c/os.d
@@ -0,0 +1,6 @@
+.\os.o: os.c
+.\os.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\os.o: os.h
+.\os.o: PLL.h
+.\os.o: tm4c123gh6pm.h
+.\os.o: PORTE.h
